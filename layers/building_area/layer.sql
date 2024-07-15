f8fbf5adbbf080a7ef310360116c6bb55138f007
Aleksandr <admin@ijo42.ru>
2024-07-15 13:25:18 +0500

    chore: add building area height

--- layers/building_area/layer.sql
+++ layers/building_area/layer.sql
@@ -2,11 +2,12 @@ CREATE OR REPLACE FUNCTION layer_building_area(bbox geometry, zoom_level integer
      RETURNS TABLE
              (
                  id       integer,
-                 geometry geometry
+                 geometry geometry,
+                 height   integer
               )
 AS
 $$
- SELECT id, geometry
+ SELECT id, geometry, height
  FROM osm_building_polygon
  WHERE zoom_level < %%VAR:indoor_zoom%% AND geometry && bbox
 $$ LANGUAGE SQL IMMUTABLE;
